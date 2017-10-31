ace90f08f8113460addc7af7e7cba9556cb19e79
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2017-10-31 16:20:01 +0100

    Small global modifications, add examples

---
+++ examples/helloworld.c
@@ -0,0 +1,15 @@
+/*
+ * theo begin
+ * run: ./helloworld
+ * output: Hello World!
+ * theo end
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World!\n");
+	return (EXIT_SUCCESS);
+};
+
--- theo.py
+++ theo.py
@@ -15,8 +15,14 @@ def readlist(dict, key):
         ret = [ret]
     return ret
 
+class TestStatus:
+    NOTRUN = 0
+    RUNNING = 1
+    PASS = 2
+    ERROR = 3
+
 class FailureReason:
-    PASS = 0
+    NONE = 0
     SETUP = 1
     TIME  = 2
     OUTPUT = 3
@@ -32,179 +38,207 @@ class Suite(object):
                 raise Exception("Input file is not YAML, error at line %d"%(e.problem_mark.line,))
 
         self.name,_ = os.path.splitext(os.path.basename(path))
-        self.parseConfiguration(suite['configuration'])
-        self.parseTests(suite['tests'])
-        self.failedTests = []
-        self.completedTests = []
+
+        self.configuration = dict()
+        self.tests = list()
+
+        if 'configuration' in suite:
+            self.parseConfiguration(suite['configuration'])
+        if 'tests' in suite:
+            self.parseTests(suite['tests'])
 
     def parseConfiguration(self, config):
-        self.environment = readlist(config, 'environment')
-        self.volatile = readlist(config, 'volatile')
+        config['environment'] = readlist(config, 'environment')
+        config['volatile'] = readlist(config, 'volatile')
+        self.configuration.update(config)
 
     def parseTests(self, configlist):
-        self.tests = []
         for config in configlist:
-            self.newTest(config)
-
-    def newTest(self, configuration):
-        self.tests.append(Test(configuration))
+            self.tests.append(Test(config, self))
 
     def runTests(self):
 
-        print '\033[7m{0:^39} {1:4} {2:4} {3:^30}\033[0m'.format('Test name','Test','Valg','Message')
+        print '\033[7m{0:39} {1:4} {2:4} {3:30}\033[0m'.format('Test name','Test','Valg','Message')
 
         for test in self.tests:
             self.setup()
             err = test.run()
-            test.run_valgrind()
-            if err:
-                self.failedTests.append(test)
-            else:
-                self.completedTests.append(test)
             self.show_test_results(test)
             self.setdown()
 
-        if len(self.failedTests) == 0:
+        failed = [test for test in self.tests if test.abortmsg != None]
+
+        if len(failed) == 0:
             msg = 'All tests completed successfully'
         else:
-            msg = 'Failed {0} tests of {1}'.format(len(self.failedTests), len(self.tests))
+            msg = 'Failed {0} tests of {1}'.format(len(failed), len(self.tests))
 
         print '\033[7m{0:^80}\033[0m'.format(msg)
 
-        for test in self.failedTests:
-            print self.name + '.' + test.name, test.showDiagnostics()
+        for test in failed:
+            print "{0}: {1}".format(test.name, test.showDiagnostics())
 
     def show_test_results(self, test):
         print '{0:39}'.format(test.name),
 
-        if test.abortmsg:
-            print "\033[7;31mFAIL\033[0m ---- {0}".format(test.abortmsg),
-        else:
-            print "\033[32m OK \033[0m",
-            if test.valgrind:
-                print "\033[31mFAIL\033[0m {0} valgrind errors".format(test.valgrind),
-            else:
-                print "\033[32m OK \033[0m",
-        print
+        if test.statusTest == TestStatus.PASS:
+            print "\033[32mPASS\033[0m",
+        elif test.statusTest == TestStatus.ERROR:
+            print "\033[31mFAIL\033[0m",
+        elif test.statusTest == TestStatus.NOTRUN:
+            print "----",
 
+        if test.statusValgrind == TestStatus.PASS:
+            print "\033[32mPASS\033[0m",
+        elif test.statusValgrind == TestStatus.ERROR:
+            print "\033[31mFAIL\033[0m",
+        elif test.statusValgrind == TestStatus.NOTRUN:
+            print "----",
+
+        if test.abortmsg:
+            print test.abortmsg,
+
+        print
 
     def setup(self):
         self.savedEnvironment = dict()
-        for pair in self.environment:
+        for pair in self.configuration['environment']:
             key,_,value = pair.partition('=')
             if key in os.environ:
                 self.savedEnvironment[key] = os.environ[key]
             os.environ[key] = value
 
-        for path in self.volatile:
+        for path in self.configuration['volatile']:
             try:
                 os.remove(path)
             except:
                 pass
 
     def setdown(self):
-        for pair in self.environment:
+        for pair in self.configuration['environment']:
             key,_,value = pair.partition('=')
             if key in self.savedEnvironment:
                 os.environ[key] = self.savedEnvironment[key]
             else:
                 del os.environ[key]
 
-        for path in self.volatile:
+        for path in self.configuration['volatile']:
             try:
                 os.remove(path)
             except:
                 pass
 
 class Test(object):
-    def __init__(self, config):
+    default_configuration = dict(
+        input= None,
+        output= None,
+        timeout= 10,
+        exit = None,
+        valgrind = False
+    )
+    def __init__(self, config, suite):
+
+        self.config = Test.default_configuration.copy()
+        self.config.update(config)
+
+        self.suite = suite
+
         self.name = config['name']
-        self.command = config['run']
-        self.setup = config['setup'] if 'setup' in config else []
-        if type(self.setup) != list:
-            self.setup = [self.setup]
-        self.input = str(config['input']) if 'input' in config else None
+        self.config['setup'] = readlist(config,'setup')
 
-        if 'output' in config:
-            self.stdout = config['output'] or ""
-        else:
-            self.stdout = None
-
-        self.retval = config['return'] if 'return' in config else None
-        self.timeout = int(config['timeout']) if 'timeout' in config else 10
+        self.statusSetup = TestStatus.NOTRUN
+        self.statusTest = TestStatus.NOTRUN
+        self.statusValgrind = TestStatus.NOTRUN
 
         self.abortmsg = None
-        self.valgrind = None
 
     def abort(self, msg, reason):
         self.abortmsg = msg
         self.reason = reason
         return msg
 
-    def run(self):
-        for command in self.setup:
+    def setup(self):
+        self.statusSetup = TestStatus.RUNNING
+        for command in self.config['setup']:
             if subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
+                self.statusSetup = TestStatus.ERROR
                 return self.abort("Setup failed", FailureReason.SETUP)
+        self.statusSetup = TestStatus.PASS
 
-        self.process = subprocess.Popen("exec " + self.command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    def run(self):
+        if self.setup():
+            return FailureReason.SETUP
+
+        self.statusTest = TestStatus.RUNNING
+        self.process = subprocess.Popen("exec " + self.config['run'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 
         def target():
-            (stdout, stderr) = self.process.communicate(self.input)
+            (stdout, stderr) = self.process.communicate(self.config['input'])
             self.runResults = (stdout, stderr)
 
         thread = threading.Thread(target=target)
         thread.start()
 
-        thread.join(self.timeout)
+        thread.join(self.config['timeout'])
         if thread.is_alive():
             process.kill()
             thread.join()
-            return self.abort("Timeout of %d seconds"%(self.timeout,), FailureReason.TIME)
+            self.statusTest = TestStatus.ERROR
+            return self.abort("Timeout of %d seconds"%(self.config['timeout'],), FailureReason.TIME)
 
-        if self.stdout != None and str(self.stdout).strip() != self.runResults[0].strip():
+        if self.config['output'] != None and str(self.config['output']).strip() != self.runResults[0].strip():
+            self.statusTest = TestStatus.ERROR
             return self.abort("Wrong output", FailureReason.OUTPUT)
 
-        if self.retval != None and self.retval != self.process.returncode:
+        if self.config['exit'] != None and self.config['exit'] != self.process.returncode:
+            self.statusTest = TestStatus.ERROR
             return self.abort("Wrong return value", FailureReason.RETURN)
 
-        return None
+        self.statusTest = TestStatus.PASS
 
-    def run_valgrind(self):
-        for command in self.setup:
-            if subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
-                return self.abort("Setup failed", FailureReason.SETUP)
+        if self.suite.configuration['valgrind'] != True:
+            return None
+
+        self.setup()
 
         xmlfile = tempfile.NamedTemporaryFile()
 
+        self.statusValgrind = TestStatus.RUNNING
         valgrindstr="valgrind --error-exitcode=127 --xml-file="+xmlfile.name+" --xml=yes --leak-check=full";
-        self.process = subprocess.Popen("exec " + valgrindstr + " " + self.command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        self.process = subprocess.Popen("exec " + valgrindstr + " " + self.config['run'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 
         def target():
-            (stdout, stderr) = self.process.communicate(self.input)
+            (stdout, stderr) = self.process.communicate(self.config['input'])
             self.runResults = (stdout, stderr)
 
         thread = threading.Thread(target=target)
         thread.start()
 
-        thread.join(self.timeout)
+        thread.join(self.config['timeout'])
         if thread.is_alive():
             process.kill()
             thread.join()
-            return self.abort("Timeout of %d seconds"%(self.timeout,), FailureReason.TIME)
+            self.statusValgrind = TestStatus.ERROR
+            return self.abort("Timeout of %d seconds"%(self.config['timeout'],), FailureReason.TIME)
 
         from lxml import etree
         tree = etree.parse(xmlfile)
         errors = tree.xpath('/valgrindoutput/error')
         if errors:
+            self.statusValgrind = TestStatus.ERROR
             self.valgrind = len(errors)
             shutil.copyfile(xmlfile.name, self.name + '.valgrind')
+        else:
+            self.statusValgrind = TestStatus.PASS
         xmlfile.close()
 
     def showDiagnostics(self):
         if self.reason == FailureReason.OUTPUT:
-            return "Expected output '{0}' but found '{1}'".format(str(self.stdout).strip(), self.runResults[0].strip())
+            return "Expected output '{0}' but found '{1}'".format(str(self.config['output']).strip(), self.runResults[0].strip())
         elif self.reason == FailureReason.RETURN:
-            return "Expected return value '{0}' but found '{1}'".format(self.retval, self.process.returncode)
+            return "Expected return value '{0}' but found '{1}'".format(self.config['exit'], self.process.returncode)
+        elif self.reason == FailureReason.SETUP:
+            return "Setup failed"
 
 def main():
     suite = Suite(sys.argv[1])
